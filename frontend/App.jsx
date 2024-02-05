3d05b96dfe380014da51fe290e7dc43baa2a8cf2
jayoon9916 <cheesen.oon.2022@scis.smu.edu.sg>
2024-02-05 18:18:16 +0800

    <feat> Add and include fonts in App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -13,7 +13,13 @@ SplashScreen.preventAutoHideAsync();
 export default function App() {
 
   const [fontsLoaded, fontError] = useFonts({
-    'frank-regular' : require('./assets/fonts/FrankRuhlLibre-Regular.ttf')
+    'frank-regular' : require('./assets/fonts/FrankRuhlLibre-Regular.ttf'),
+    'frank-light' : require('./assets/fonts/FrankRuhlLibre-Light.ttf'),
+    'frank-medium' : require('./assets/fonts/FrankRuhlLibre-Medium.ttf'),
+    'frank-bold' : require('./assets/fonts/FrankRuhlLibre-Bold.ttf'),
+    'frank-black' : require('./assets/fonts/FrankRuhlLibre-Black.ttf'),
+    'arapey-italic' : require('./assets/fonts/Arapey-Italic.ttf'),
+    'arapey-regular' : require('./assets/fonts/Arapey-Regular.ttf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
