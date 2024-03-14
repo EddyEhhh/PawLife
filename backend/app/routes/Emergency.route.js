047986ae5f690c58aa3e6ee2fc0201563add3420
Stabb <Stabbershade@hotmail.com>
2024-03-15 02:17:53 +0800

    <feat> New routes

--- backend/app/routes/Emergency.route.js
+++ backend/app/routes/Emergency.route.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import { getDemo } from "../controllers/App.controller.js";
-import {getEmergency, postEmergency , getUpcomingEmergency} from "../controllers/Emergency.controller.js";
+import {getEmergency, postEmergency} from "../controllers/Emergency.controller.js";
 
 const router = express.Router();
 
 router.get('/', getEmergency);
-router.get('/upcoming', getUpcomingEmergency); // Get upcoming appointments in HomePage
 router.post('/', postEmergency);
 
 export default router;
--- backend/app/routes/Pet.route.js
+++ backend/app/routes/Pet.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getDemo } from "../controllers/App.controller.js";
 import {getProfile} from "../controllers/User.controller.js";
-import {getPets, postPets} from "../controllers/Pet.controller.js";
+import {getPets, getPetsById, postPets} from "../controllers/Pet.controller.js";
 
 const router = express.Router();
 
 router.get('/', getPets);
+router.get('/pet', getPetsById);
 router.post('/', postPets);
 export default router;
 
